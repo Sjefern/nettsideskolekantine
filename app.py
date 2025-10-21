9017c09feabee72ad111a33c23d4f5d2f46f025e
Sjefern <sebastiankjos08@gmail.com>
2025-10-21 14:40:44 +0200

    Har lagt til ister med ordbøker og sender de til html sidene. har lagt til bilder i listene som tilhørere listen. har lagt til meny og vare liste til kantinen.

--- app.py
+++ app.py
@@ -8,15 +8,33 @@ def index():
 
 @app.route('/varer')
 def varer():
-    return render_template("varer.html")
+    varerer = [
+        {"navn": "Toast", "pris": 30, "bilde": "/static/images/Toast.jpg"},
+        {"navn": "sjokolademelk", "pris": 30, "bilde": "/static/images/sjokolademelk.jpeg"},
+        {"navn": "Proteinbar", "pris": 35, "bilde": "/static/images/Proteinbar.webp"}, 
+        {"navn": "Cola", "pris": 25, "bilde": "/static/images/Cola.jpg"},
+        {"navn": "Eplejuice", "pris": 15, "bilde": "/static/images/eplejuice.jpeg"}
+    ]
+    return render_template("varer.html", varere=varerer)
 
 @app.route('/meny')
 def meny():
-    return render_template("meny.html")
+    menyer = [
+        {"dag": "Mandag", "dagens": "Tomat suppe", "bilde": "/static/images/tomatsuppe.jpeg"},
+        {"dag": "Tirsdag", "dagens": "Lasgne", "bilde": "/static/images/lasagne.jpg"},
+        {"dag": "Onsdag", "dagens": "Fiske kaker", "bilde": "/static/images/fiskekaker.jpeg"},
+        {"dag": "Torsdag", "dagens": "Haifinnsuppe", "bilde": "/static/images/haifinnesuppe.jpg"},
+        {"dag": "Fredag", "dagens": "Taco", "bilde": "/static/images/taco.jpeg"}
+    ]
+    return render_template("meny.html", menyer=menyer)
 
 @app.route('/kontakt')
 def kontakt():
-    return render_template("kontakt.html")
+    kontakter = [
+        {"person": "Sebastian", "email": "sebastian@hotmail.com", "nummer": "+ 47 901 33 902"},
+        {"person": "Marcus", "email": "marcus@hotmail.com", "nummer": "+ 47 902 44 903"}
+    ]
+    return render_template("kontakt.html", kontakter=kontakter)
 
 
 if __name__ == '__main__':
